f4d49d1b83277c18287a9bea118f6d2eb7725c4f
nmihayl <nikola_mihaylov@gmx.de>
2024-05-21 11:16:26 +0200

    Database update - bomberman

--- gamelist.php
+++ gamelist.php
@@ -13,13 +13,9 @@
     <meta property="og:type" content="game" />
     <meta property="og:url" content="http://www.igraiigri.com/igrai/Online+%D0%98%D0%B3%D1%80%D0%B8" />
     <meta property="og:image" content="http://www.igraiigri.com/screenshots/90_4536.jpg" />
-    <meta property="og:description"
-        content="Тук ще намериш всички други онлайн игри, които не са подходящи за другите категории. " />
 
     <link rel="stylesheet" href="../css/style.css" type="text/css" />
     <link rel="stylesheet" media="only screen and (min-width: 1260px)" href="../css/big.css" />
-
-
 
     <link rel="shortcut icon" type="image/x-icon" href="../favicon.ico" />
 
@@ -41,32 +37,6 @@
                         <input type="image" id="searchButton" src="../images/search.png" width="32" height="32"
                             border="0" ALT="Търси!">
                     </form>
-                </div>
-                <div id="dropdown">
-                    <ul>
-                        <li><a class="masterDropdown" href="../index.html">Igri</a>
-                            <ul>
-                                <li><a href="Адвенчър+Игри.html">Адвенчър Игри</a></li>
-                                <li><a href="Летящи+игри.html">Летящи игри</a></li>
-                                <li><a href="Забавни+игри.html">Забавни Игри</a></li>
-                                <li><a href="Игри+в+мултиплеар.html">Игри в мултиплеар</a></li>
-                                <li><a href="Логически+игри.html">Логически Игри</a></li>
-                                <li><a href="Състезателни+игри.html">Състезателни Игри</a></li>
-                                <li><a href="Трудни+игри.html">Трудни Игри</a></li>
-                                <li><a href="Спортни+игри.html">Спортни Игри</a></li>
-                                <li><a href="Платформени+игри.html">Платформени Игри</a></li>
-                                <li><a href="Момичешки+игри.html">Момичешки Игри</a></li>
-                                <li><a href="Телевизия+&amp;+Филми.html">Телевизия & Филми</a></li>
-                                <li><a href="+Игри+на+даска+и+Карти.html"> Игри на даска и Карти</a></li>
-                                <li><a href="Екшън+Игри.html">Екшън Игри</a></li>
-                                <li><a href="Детски+игри.html">Детски Игри</a></li>
-                                <li><a href="Online+Игри.html">Online Игри</a></li>
-                                <li><a href="Класически+игри.html">Класически игри</a></li>
-                                <li><a href="Симулации.html">Симулации</a></li>
-                            </ul>
-
-                        </li>
-                    </ul>
                 </div>
             </div>
         </div>
